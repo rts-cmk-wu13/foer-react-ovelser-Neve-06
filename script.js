21f64d9f1cf145b3293d552786a58d4d66aab386
Neve-06 <6255234@rts-365.dk>
2025-05-12 09:21:59 +0200

    Opgave 2 complete!

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let dog1 =  dogs[1]
 let dog2 = dogs[2]
 
 console.log(dog1, dog2);
+
+
+//Opgave 2
+const catName = cat.name;
+const catBreed = cat.breed;
+const catMail = cat.owner.contact;
+
+console.log(catName, catBreed, catMail);
